aca4741515a6ccec406824b40c0a7a07d99a8081
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-10 17:59:49 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word(message)
-  split_phrase = message.split
-  reversed = []
-  split_phrase.each do |word|
-    reversed << word.reverse
+  reversed = message.split
+  reversed.collect do |word|
+    word.reverse
   end
-  reversed = reversed.join(" ")
+  reversed = reversed.join
 end
